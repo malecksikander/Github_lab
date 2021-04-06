41e2b01215c24f1f8a7dc54530e1be3d313f7fef
Malik Sikander <bse183887@cust.pk>
2021-04-06 15:28:56 +0500

    i inserted second line

---
+++ src/Sample_Class.java
@@ -0,0 +1,8 @@
+public class Sample_Class {
+    Sample_Class()
+    {
+        System.out.println("Sampe Class");
+
+        System.out.println("Change in this File after Uploading project");
+    }
+}
